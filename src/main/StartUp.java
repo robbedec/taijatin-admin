5a36e698a6f19919cc4c43813c05191e2b8c213e
Robbe Decorte <robbe.decorte@student.hogent.be>
2019-03-30 18:42:37 +0100

    database connection with jtds

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -1,14 +1,29 @@
 package main;
 
-import cui.ConsoleMenu;
-import domain.DomainController;
+import domain.Address;
+import util.JPAUtil;
 
-import java.util.Scanner;
+import javax.persistence.EntityManager;
 
 public class StartUp {
     public static void main(String[] args) {
-        DomainController dc = new DomainController();
-        Scanner input = new Scanner(System.in);
-        ConsoleMenu menu = new ConsoleMenu(dc, input);
+        System.out.println("robbe");
+
+        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Address a = new Address();
+        a.setCity("brugge");
+       // a.setAddressId(10);
+        a.setCountry("belige");
+        a.setNumber(5);
+        a.setStreet("kkkk");
+        a.setZipCode(8888);
+       //a.setAddressId(100);
+        entityManager.persist(a);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        JPAUtil.getEntityManagerFactory().close();
+        //System.out.println(us.getUserName());
     }
 }

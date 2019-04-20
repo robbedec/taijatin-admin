e57d1f41d76c1666a17fe8a6bc922a79c3ec74b8
jonasbaert <jonas.baert.w0188@student.hogent.be>
2019-04-20 10:21:46 +0200

    Added some more fields, the dates also works already, but the choiceBoxes not yet

--- src/gui/DetailPanelController.java
+++ src/gui/DetailPanelController.java
@@ -5,17 +5,28 @@ import domain.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 
 public class DetailPanelController extends GridPane implements PropertyChangeListener {
     private DomainController dc;
     @FXML
     private TextField txtUsername, txtFirstname, txtLastname, txtEmail;
+    @FXML
+    private DatePicker txtBirthday, txtRegistrationDate;
+    @FXML
+    private ChoiceBox txtGrade, txtGender, txtType;
     @FXML
     private Button btnSave;
     private User user;
@@ -36,6 +47,8 @@ public class DetailPanelController extends GridPane implements PropertyChangeLis
         user.setFirstname(txtFirstname.getText());
         user.setLastname(txtLastname.getText());
         user.setEmail(txtEmail.getText());
+        user.setBirthday((java.sql.Date) convertLocalDateToDate(txtBirthday));
+        user.setBirthday((java.sql.Date) convertLocalDateToDate(txtRegistrationDate));
         dc.setCurrentUser(user);
         dc.updateUser();
     }
@@ -48,6 +61,17 @@ public class DetailPanelController extends GridPane implements PropertyChangeLis
             txtFirstname.setText(user.getFirstname());
             txtLastname.setText(user.getLastname());
             txtEmail.setText(user.getEmail());
+            txtBirthday.setValue(user.getBirthday().toLocalDate());
+            txtRegistrationDate.setValue(user.getRegistrationdate().toLocalDate());
         }
     }
+
+    //Convert localdate to date
+    private Date convertLocalDateToDate(DatePicker datePicker){
+        LocalDate localDate = datePicker.getValue();
+        Instant instant = Instant.from(localDate.atStartOfDay(ZoneId.systemDefault()));
+        Date date = Date.from(instant);
+        return date;
+    }
 }
+;

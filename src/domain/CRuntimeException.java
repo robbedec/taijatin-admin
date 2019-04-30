75b369d3425007413895577a6e806cde1a77168b
edwardkerckhof <edwardkerckhof@hotmail.com>
2019-04-30 17:59:48 +0200

    Basic validation via dto, email and NationalInsuranceNumber work with regex patterns

---
+++ src/domain/CRuntimeException.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class CRuntimeException extends java.lang.RuntimeException {
+    public CRuntimeException() {}
+    public CRuntimeException(String message) { super(message); }
+}
--- src/domain/Club.java
+++ src/domain/Club.java
@@ -97,7 +97,6 @@ public class Club {
             userDTOLijst.add(newUserDTO);
         }
 
-
     }
 
     public void deleteUser(){
